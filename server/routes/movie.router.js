b7ea277f5f6ff64ccde9aa9a8a35abb4db44f53b
Peter Hagenson <p.hagenson@gmail.com>
2022-08-26 16:40:04 -0500

    detailMovie query tested and working

--- server/routes/movie.router.js
+++ server/routes/movie.router.js
@@ -19,6 +19,21 @@ router.get('/', (req, res) => {
 //create GET for individual movie
 router.get('/:id', (req, res) => {
 console.log('in detail get', req.params.id)
+const query = `SELECT "movies".*, "genres".name FROM "movies"
+RIGHT JOIN "movies_genres"
+ON "movies".id = "movies_genres".movie_id
+ JOIN "genres"
+ON "movies_genres".genre_id = "genres".id
+WHERE "movies".id = $1`;
+pool.query(query, [req.params.id])
+    .then( result => {
+      res.send(result.rows);
+      console.log(result.rows)
+    })
+    .catch(err => {
+      console.log('ERROR: Get all movies', err);
+      res.sendStatus(500)
+    })
 })
 
 
