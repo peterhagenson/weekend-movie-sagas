72836627134c53fdc9fa5b8b343d52d1ca6b0b04
Peter Hagenson <p.hagenson@gmail.com>
2022-08-26 02:39:21 -0500

    navToHome button functional

--- src/components/MovieDetail/MovieDetail.jsx
+++ src/components/MovieDetail/MovieDetail.jsx
@@ -1,11 +1,21 @@
 import { HashRouter as Router, Route } from 'react-router-dom';
+import { useHistory } from 'react-router-dom'
+
 
 
 function MovieDetail() {
+
+    const history = useHistory();
+
+    const navToHome = () => {
+        history.push('/')
+    }
+
     return (
         <Router >
             <Route path="/details">
                 <p>detail page</p>
+                <button onClick={navToHome}>Back to List</button>
             </Route>
         </Router>
     )
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import './MovieList.css'
 import MovieDetail from '../MovieDetail/MovieDetail'
+import { useHistory } from 'react-router-dom'
 
 //WILL NEED USE HISTORY TO SEND TO MOVIE DETAIL I THINK
 //add a click events that directs user to details page
@@ -9,12 +10,18 @@ import MovieDetail from '../MovieDetail/MovieDetail'
 
 function MovieList() {
 
+    const history = useHistory();
+
     const dispatch = useDispatch();
     const movies = useSelector(store => store.movies);
 
     useEffect(() => {
         dispatch({ type: 'FETCH_MOVIES' });
     }, []);
+
+    const toDetails = () => {
+        history.push('/details');
+    }
 
     return (
         <main>
@@ -24,7 +31,7 @@ function MovieList() {
                     return (
                         <div key={movie.id} >
                             <h3>{movie.title}</h3>
-                            <img src={movie.poster} alt={movie.title} />
+                            <img src={movie.poster} alt={movie.title} onClick={toDetails} />
                         </div>
                     );
                 })}
